429b5428dff7312915ac8fd4ce8a3e4de2950fbf
Kirill Kleymenov <k.kelolov@gmail.com>
2022-12-01 16:45:29 +0400

    first

---
+++ dist/01/index.js
@@ -0,0 +1,19 @@
+import fs from 'fs';
+const file = fs.readFileSync('./src/01/input.txt').toString();
+const elves = file
+    .split('\n\n')
+    .map((el) => el.split('\n'))
+    .map((el) => el.reduce((a, b) => {
+    return Number(a || 0) + Number(b || 0);
+}, 0));
+elves.sort((a, b) => {
+    return b - a;
+});
+const main = () => {
+    console.log('first');
+    console.log(elves[0]);
+    console.log('second');
+    console.log(elves[0] + elves[1] + elves[2]);
+};
+export default main;
+//# sourceMappingURL=index.js.map
---
+++ dist/index.js
@@ -0,0 +1,3 @@
+import main from './01/index.js';
+main();
+//# sourceMappingURL=index.js.map
